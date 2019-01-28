d0f09a6643effb7600790f0d8a8cc4892a8fa94b
Clement Y <aka_clement166@hotmail.com>
2019-01-28 21:59:14 +1100

    Combining with rest of code
    remove draw section, and enemy creation putting them inside shooter_gui and shooter_main respectively.

--- shooter_main.py
+++ shooter_main.py
@@ -5,6 +5,7 @@ shooter game frame.
 import shooter_player
 import shooter_platform
 import shooter_gui
+import shooter_enemy.py
 
 WIDTH = 1200
 HEIGHT = 675
@@ -17,5 +18,15 @@ PLAT_MAP1 = [
     ,[8, 7], [8, 8], [8, 9], [8, 10]]
 PLATFORM_GROUP = shooter_platform.create_platforms(PLAT_MAP1)
 
+# initialize enemy
+colour_list = COLOR_LIST = ["Red", "Green", "Yellow", "Purple"]
+enemy_speed = [10, 20]
+enemy_list = []
+for i in range(10):
+    e = enemy([RIGHT_WALL, BOTTOM_WALL], random.choice(colour_list), 50, 10)
+    enemy_list.append(e)
+
+
+
 # initialise GUI with player and platform_group object
-GUI = shooter_gui.GUI(PLAYER, PLATFORM_GROUP)
+GUI = shooter_gui.GUI(PLAYER, PLATFORM_GROUP, enemy_list, enemy_speed)
